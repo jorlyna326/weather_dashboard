1a7f0a5ce0b4c9a20b0174b0e35602ea2215451c
Jorlyna Pujols <jorlynapujols@Jorlynas-MBP.attlocal.net>
2023-10-25 04:23:09 -0400

    added additional span items and css

--- js/script.js
+++ js/script.js
@@ -7,33 +7,59 @@ function captureCity(event) {
     const userInput = document.querySelector('#input').value;
     // // this is the value of what the user typed in
     fetchWeather(userInput);
+    fetchForecast(userInput)
     console.log("test")
 }
 
 // TODO: create a function that will fetch the data from the weather API
 function fetchWeather(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey).then(function (response) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apikey).then(function (response) {
         return response.json()
     }).then(function (data) {
         temp = data.main.temp;
+        wind = data.wind.speed;
+        humidity = data.main.humidity;
         console.log(data);
-        let lon = data.coord.lon;
-        let lat = data.coord.lat;
-        let url_weather = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apikey}`;
-        fetch(url_weather).then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            console.log(data)
-            displayWeather(data)
-        })
+            displayWeather(data);         
+    })
+
+}
+function fetchForecast(city){
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apikey).then(function (response) {
+        return response.json()
+    }).then(function (data) {
+       var forecastDisplay = document.querySelector('.display')
+        console.log(data);
+     var forecastArray = [data.list[0], data.list[7], data.list[15], data.list[23], data.list[32]]
+        forecastDisplay.innerHTML = ''
+        for (let index = 0; index < 5; index++) {
+
+            var div = document.createElement('div')
+            div.setAttribute('class', "card col-2 mx-1")
+            
+           div.innerHTML = `
+           <h4 class="cardOneDate">day one</h4>
+           <img src=" https://openweathermap.org/img/wn/02d@2x.png" alt="weather_icon">
+           <p>Temperature <span class="cardOneTemp">${forecastArray[index].main.temp}</span></p>
+           <p>Wind <span class="cardOneWind">${forecastArray[index].wind.speed}</span></p>
+           <p>Humidity <span class="cardOneHumid">${forecastArray[index].main.humidity}</span></p>
+         `
+            
+         forecastDisplay.append(div) 
+        }
+        
         
     })
 }
+
 function displayWeather(data){
-    let currentTemp = data.current.temp
-    let currentWind = data.current.wind_speed
-    let currentHum  = data.current.humidity
-    $("#Temp").text()
+    let currentTemp = data.main.temp
+    let currentWind = data.wind.speed
+    let currentHum  = data.main.humidity
+    document.querySelector(".temp").textContent = `${currentTemp} degrees`;
+    document.querySelector(".wind").textContent =`${currentWind} mph`;
+    document.querySelector(".humidity").textContent =`${currentHum} %`
+    document.querySelector("#icon").setAttribute('src', "https://openweathermap.org/img/wn/"+ data.weather[0].icon +"@2x.png")
 }
 
 
